b85ff2cb9eb77bea66104730fd56fddb31a992a1
hieunguyen <hieund76134@gmail.com>
2021-11-06 17:52:58 +0700

    install scipt

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash#
+
+brew install iterm2
+brew install flutter
+brew install go
+brew install nvm
+brew install neovim
+brew install vscode
+brew install rustup
+rustup-init
---
+++ install_path.sh
@@ -0,0 +1,5 @@
+#!/bin/bash#
+
+ln -s ~/repos/github/flutter/bin/flutter /usr/local/bin/flutter
+ln -s ~/repos/github/flutter/bin/dart /usr/local/bin/dart
+ln -s ~/go/bin/* /usr/local/bin
