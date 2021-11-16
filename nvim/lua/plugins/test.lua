e2f29c3fc7f800f738d1c4a413b6193062d1dcb7
hieunguyen <hieund76134@gmail.com>
2021-11-16 20:06:02 +0700

    mod vim-test term pos

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -5,6 +5,7 @@ g['test#go#runner'] = 'gotest'
 g['test#neovim#start_normal'] = 1
 g['test#strategy'] = 'neovim'
 g['test#preserve_screen'] = 1
+g['test#neovim#term_position'] = 'vert'
 g['test#go#gotest#options'] = {
   nearest = '-v',
 }
