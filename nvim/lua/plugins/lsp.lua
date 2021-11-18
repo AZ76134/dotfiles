6e50bd821943daa4fd075c4314ecc4c50d1e4378
hieunguyen <hieund76134@gmail.com>
2021-11-18 08:56:15 +0700

    change trouble diag mode

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -115,7 +115,7 @@ trouble.setup{
     height = 10, -- height of the trouble list when position is top or bottom
     width = 50, -- width of the list when position is left or right
     icons = true, -- use devicons for filenames
-    mode = "lsp_workspace_diagnostics", -- "lsp_workspace_diagnostics", "lsp_document_diagnostics", "quickfix", "lsp_references", "loclist"
+    mode = "lsp_document_diagnostics", -- "lsp_workspace_diagnostics", "lsp_document_diagnostics", "quickfix", "lsp_references", "loclist"
     -- fold_open = "?", -- icon used for open folds
     -- fold_closed = "?", -- icon used for closed folds
     group = true, -- group results by file
@@ -146,7 +146,7 @@ trouble.setup{
     auto_close = false, -- automatically close the list when you have no diagnostics
     auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
     auto_fold = false, -- automatically fold a file trouble list at creation
-    auto_jump = {"lsp_definitions"}, -- for the given modes, automatically jump if there is only a single result
+    -- auto_jump = {"lsp_definitions"}, -- for the given modes, automatically jump if there is only a single result
     -- signs = {
     --     -- icons / text used for a diagnostic
     --     error = "?",
