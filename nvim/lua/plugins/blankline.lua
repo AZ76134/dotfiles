3f5f4f0eace0a64bdc0561f70e22a0908490ca9c
hieunguyen <hieund76134@gmail.com>
2021-11-20 17:00:28 +0700

    change blankline setting

--- nvim/lua/plugins/blankline.lua
+++ nvim/lua/plugins/blankline.lua
@@ -1,22 +1,23 @@
 local ok, blankline = pcall(require, "indent_blankline")
 if (not ok) then return end
+
 blankline.setup {
-  -- char = "|",
- space_char_blankline = " ",
- show_current_context = true,
- show_current_context_start = true,
- show_first_indent_level = false,
-  filetype_exclude = {
-    'help',
-    'git',
-    'markdown',
-    'text',
-    'terminal',
-    'packer',
-    'dashboard'
-  },
-  buftype_exclude = {
-    'terminal',
-    'nofile'
-  },
+	space_char_blankline = " ",
+	show_current_context = true,
+	show_current_context_start = true,
+	show_first_indent_level = false,
+	filetype_exclude = {
+		'help',
+		'git',
+		'markdown',
+		'text',
+		'terminal',
+		'packer',
+		'dashboard',
+		'Trouble',
+	},
+	buftype_exclude = {
+		'terminal',
+		'nofile'
+	},
 }
