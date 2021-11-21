f5c433a13b4ae7d178eed00675a3c760696aab2a
hieunguyen <hieund76134@gmail.com>
2021-11-21 17:38:21 +0700

    mod escape map

--- nvim/lua/plugins/escape.lua
+++ nvim/lua/plugins/escape.lua
@@ -2,7 +2,7 @@ local ok, escape = pcall(require, "better_escape")
 if (not ok) then return end
 
 escape.setup ({
-  mapping = { "jk", "kj", "jj", "kk" },
+  mapping = { "jk", "kj" },
   timeout = vim.o.timeoutlen,
   clear_empty_lines = false,
   keys = "<Esc>",
